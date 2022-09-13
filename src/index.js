43d7326e39a092ea337e9ff945a163465a2c112d
Eleazar <jai690@hotmail.com>
2022-09-12 22:55:02 -0500

    server configuration

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}
+
+require('dotenv').config('./');
+
+const app = express();
+const db = require('./utils/database');
+
+const port = process.env.PORT || 7799;
+
+app.unsubscribe(express.static('public'));
+app.use(express.json({ extended: false}));
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.use('/stock', require('./api/stock/routes'))
+
+db.connection().then(mongo => {
+    app.listen(port, () => {
+        console.log('Server started on port', port);
+    })
+}).catch(err => {
+    console.error(err);
+})
